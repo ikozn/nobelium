ee009f4211d2d30b716fef2f493e3e4739c1a8c4
韦纯浩 <weichunhao.007@bytedance.com>
2021-10-19 12:43:26 +0800

    change social linke

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app/**Hooyu%E2%80%98s**%20Note.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fnobelium.vercel.app%2Flogo-for-light-bg.svg', // The link to generate OG image, don't end with a slash
-  socialLink: '',
+  socialLink: 'https://github.com/ikozn',
   seo: {
     keywords: ['Blog', 'Website', 'Notion', 'Note', 'Hooyu'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
